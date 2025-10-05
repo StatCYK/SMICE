c14cd80c0863ea8d3c93f08f24cfec96283e5565
Yongkai Chen <70113196+StatCYK@users.noreply.github.com>
2025-10-05 13:28:26 -0400

    Update run_SMICE_all.sh

--- bash/benchmark_exp/run_SMICE_all.sh
+++ bash/benchmark_exp/run_SMICE_all.sh
@@ -11,7 +11,6 @@ for ((i=0; i<${#jobnames[@]}; i+=CHUNK_SIZE)); do
     # Extract chunk of jobnames
     chunk=("${jobnames[@]:i:CHUNK_SIZE}")
     jobname="${chunk[0]}"  # Since CHUNK_SIZE=1
-    
     # Submit the first job and capture its job ID
     jobid1=$(sbatch -J "$jobname" run_SS_AFpred.slurm "${chunk[@]}" | awk '{print $4}')
     echo "Submitted job '$jobname' with job ID $jobid1"
@@ -21,8 +20,8 @@ for ((i=0; i<${#jobnames[@]}; i+=CHUNK_SIZE)); do
     echo "Submitted dependent job '${jobname}_enhancedSamp' after job ID $jobid1"
     
     # Submit the second job with dependency on the first
-    sbatch --dependency=afterok:$jobid2 -J "${jobname}_enhancedSamp" run_Rep_extract.slurm "${chunk[@]}"
-    echo "Submitted dependent job '${jobname}_enhancedSamp' after job ID $jobid1"
+    sbatch --dependency=afterok:$jobid2 -J "${jobname}_RepExtract" run_Rep_extract.slurm "${chunk[@]}"
+    echo "Submitted dependent job '${jobname}_RepExtract' after job ID $jobid2"
     
     # Small delay to avoid overwhelming the scheduler
     sleep 1
