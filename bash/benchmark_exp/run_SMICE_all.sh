bfb70b120dafec841747852c329537f9e1fd4bf2
Yongkai Chen <70113196+StatCYK@users.noreply.github.com>
2025-10-05 21:42:10 -0400

    Update run_SMICE_all.sh

--- bash/benchmark_exp/run_SMICE_all.sh
+++ bash/benchmark_exp/run_SMICE_all.sh
@@ -15,13 +15,17 @@ for ((i=0; i<${#jobnames[@]}; i+=CHUNK_SIZE)); do
     jobid1=$(sbatch -J "$jobname" run_SS_AFpred.slurm "${chunk[@]}" | awk '{print $4}')
     echo "Submitted job '$jobname' with job ID $jobid1"
     
-    # Submit the second job with dependency on the first
+    # Submit the enhanced sampling with dependency on the prev step
     jobid2=$(sbatch --dependency=afterok:$jobid1 -J "${jobname}_enhancedSamp" run_enhancedSamp.slurm "${chunk[@]}" | awk '{print $4}')
     echo "Submitted dependent job '${jobname}_enhancedSamp' after job ID $jobid1"
     
-    # Submit the second job with dependency on the first
-    sbatch --dependency=afterok:$jobid2 -J "${jobname}_RepExtract" run_Rep_extract.slurm "${chunk[@]}"
-    echo "Submitted dependent job '${jobname}_RepExtract' after job ID $jobid2"
+    # Submit the full msa prediction with dependency on the prev step
+    jobid3=$(sbatch --dependency=afterok:$jobid2 -J "${jobname}_fullPred" run_colabFold_full.slurm "${chunk[@]}"| awk '{print $4}')
+    echo "Submitted dependent job '${jobname}_fullPred' after job ID $jobid2"
+    
+    # Submit the second job with dependency on the prev step
+    jobid4=$(sbatch --dependency=afterok:$jobid3 -J "${jobname}_RepExtract" run_Rep_extract.slurm "${chunk[@]}"| awk '{print $4}')
+    echo "Submitted dependent job '${jobname}_RepExtract' after job ID $jobid3"
     
     # Small delay to avoid overwhelming the scheduler
     sleep 1
